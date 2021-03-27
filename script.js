31a2acefbb63a9b2ad0a6b1ddd51943bebd7e7f2
GyaJD <73749198+GyaJD@users.noreply.github.com>
2021-03-27 17:42:09 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+const leftArrow = document.getElementsByClassName("left")[0];
+const rightArrow = document.getElementsByClassName("right")[0];
+
+const slides = document.getElementsByClassName("slide");
+console.log(slides);
